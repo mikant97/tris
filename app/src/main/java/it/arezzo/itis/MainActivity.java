42f9063208bd6e3bbc76b3b1eb5df7e6ff98ecac
mikant97 <guerra.michele@hotmail.it>
2023-01-07 11:58:12 +0100

    Tris: Mancano controllo finali

---
+++ app/src/main/java/it/arezzo/itis/MainActivity.java
@@ -0,0 +1,186 @@
+package it.arezzo.itis;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button[][] sca = new Button[3][3];
+    int[][] mat = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+    };
+    Button[] vet = new Button[9];
+    boolean qualeChr = true;  // true = X false 0
+    boolean giocatore = true; // true Human false WOPR
+    int r = 0;
+    int c = 0;
+    String gioca1;
+    String gioca2;
+    int pareggio = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        giocatore = Presentazione.inizia;
+        Bundle dati = getIntent().getExtras();
+        gioca1 = dati.getString("Gio1");
+        gioca2 = dati.getString("Gio2");
+        caricaButton();
+        Button gioca = (Button) findViewById(R.id.ButtonGioca);
+        if (giocatore) { gioca.setText("Tocca a: " + gioca1);} else { gioca.setText("Tocca a" + gioca2); }
+
+        for (int i = 0, k = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                sca[i][j] = vet[k];
+                String app = "Bottone_" + i + "_" + j;
+                sca[i][j].setTag(app);
+                sca[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String app = (String) view.getTag();
+                        String[] app1 = app.split("_");
+                        r = Integer.parseInt(app1[1]);
+                        c = Integer.parseInt(app1[2]);
+                        if (!((sca[r][c].getText().toString().equals("O")) || (sca[r][c].getText().toString().equals("X")))) {
+                            if (qualeChr) {
+                                sca[r][c].setText("X");
+                                qualeChr = false;
+                            } else {
+                                sca[r][c].setText("O");
+                                qualeChr = true;
+                            }
+                        }
+                        if (giocatore) {
+                            mat[r][c] = 1;
+                        } else {
+                            mat[r][c] = 2;
+                        }
+                        if (controllo(giocatore)) {
+                            if (giocatore) {
+                                gioca.setText("Ha vinto : " + gioca1 + " !!!!");
+                            } else {
+                                gioca.setText("Ha vinto : " + gioca2 + " !!!!");
+                            }
+                        } else {
+                            if (pareggio == 9) {  gioca.setText("Pareggio ! Tris di solito è così");
+                            } else {
+                                if (giocatore) {
+                                    giocatore = false;
+                                } else {
+                                    giocatore = true;
+                                }
+                            }
+                        }
+
+
+                        // gioca.setText("Hai fatto click su button: i: " + app1[1] + " j: " + app1[2]);
+                    }
+                });
+                k++;
+            }
+    }
+
+    public void principale(View view) {
+        Intent princip = new Intent(MainActivity.this, Presentazione.class);
+        startActivity(princip);
+    }
+
+    public void caricaButton() {
+        vet[0] = (Button) findViewById(R.id.button0);
+        vet[1] = (Button) findViewById(R.id.button1);
+        vet[2] = (Button) findViewById(R.id.button2);
+        vet[3] = (Button) findViewById(R.id.button3);
+        vet[4] = (Button) findViewById(R.id.button4);
+        vet[5] = (Button) findViewById(R.id.button5);
+        vet[6] = (Button) findViewById(R.id.button6);
+        vet[7] = (Button) findViewById(R.id.button7);
+        vet[8] = (Button) findViewById(R.id.button8);
+    }
+
+
+    public boolean controllo(boolean giocatore) {
+        int chrControllo;
+        if (giocatore) {
+            chrControllo = 1;
+        } else {
+            chrControllo = 2;
+        }
+        boolean vitt = false;
+        // Controllo Orizzontale
+        int vittoria = 0;
+        for (int i = 0; (i < mat.length) && !vitt; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (chrControllo == mat[i][j]) {
+                    vittoria++;
+                }
+            }
+
+            if (vittoria == 3) {
+                vitt = true;
+            } else {
+                vittoria = 0;
+            }
+        }
+        // Controllo Verticale
+        for (int i = 0; (i < mat.length) && !vitt; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (chrControllo == mat[j][i]) {
+                    vittoria++;
+                }
+            }
+            if (vittoria == 3) {
+                vitt = true;
+            } else {
+                vittoria = 0;
+            }
+        }
+        // Controllo Diagonale Principale
+        for (int i = 0; (i < mat.length) && !vitt; i++) {
+            if (chrControllo == mat[i][i]) {
+                vittoria++;
+            }
+        }
+        if (vittoria == 3) {
+            vitt = true;
+        } else {
+            vittoria = 0;
+        }
+        // Controllo Diagonale Secondaria
+        for (int i = 0; (i < mat.length) && !vitt; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (i + j == mat.length - 1) {
+                    if (chrControllo == mat[i][j]) {
+                        vittoria++;
+                    }
+                }
+            }
+        }
+        if (vittoria == 3) {
+            vitt = true;
+        } else {
+            for (int i = 0; (i < mat.length) && !vitt; i++) {
+                for (int j = 0; j < mat[0].length; j++) {
+
+                    if (mat[i][j] == 0) pareggio++;
+                }
+            }
+        }
+
+        if (vitt) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/it/arezzo/itis/Presentazione.java
@@ -0,0 +1,84 @@
+package it.arezzo.itis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Presentazione extends AppCompatActivity {
+
+    static boolean inizia = true; // true giocatore 1 false giocatore 2
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentazione);
+        Button b2 = (Button) findViewById(R.id.bttGioca);
+        b2.setEnabled(false);
+        Button b1 = (Button) findViewById(R.id.BttEstrai);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText gio1 = (EditText) findViewById(R.id.EdtGiocatore1);
+                if (gio1.getText().toString().equals("") || gio1.getText().toString().equals(null))
+                {
+                    gio1.setError(("Nome please...."));
+                    return;
+                }
+                EditText gio2 = (EditText) findViewById(R.id.edtGiocatore2);
+                if (gio2.getText().toString().equals("") || gio2.getText().toString().equals(null))
+                {
+                    gio2.setError(("Nome please...."));
+                    return;
+                }
+                if (gio1.getText().toString().toUpperCase().equals(gio2.getText().toString().toUpperCase()))
+                {
+                    gio2.setError(("Nomi diversi please...."));
+                    return;
+                }
+
+                gio1.setEnabled(false);
+                gio2.setEnabled(false);
+                int max = 2;
+                int min = 1;
+                int range = max - min + 1;
+                int rand = 0;
+                TextView ini1 = (TextView) findViewById(R.id.textView4);
+                for (int i = 0; i < 50; i++)
+                {
+                    rand = (int)(Math.random() * range) + min;
+
+                    if (rand == 1) { ini1.setText("Inizia: " + gio1.getText().toString()); }
+
+                    else {
+                        ini1.setText("Inizia: " + gio2.getText().toString());
+                    }
+                }
+                if (rand==1) { inizia = true;} else {inizia = false;}
+                b2.setEnabled(true);
+            }
+        });
+
+    }
+
+    public void lancia(View view)
+    {
+        EditText gio1 = (EditText) findViewById(R.id.EdtGiocatore1);
+        EditText gio2 = (EditText) findViewById(R.id.edtGiocatore2);
+        gio1.setEnabled(false);
+        gio2.setEnabled(false);
+        // definisco l'intenzione di aprire l'Activity "Page1.java"
+        Intent openPage1 = new Intent(Presentazione.this,MainActivity.class);
+        // passo all'attivazione dell'activity page1.java
+        openPage1.putExtra("Gio1", gio1.getText().toString());
+        openPage1.putExtra("Gio2", gio2.getText().toString());
+        startActivity(openPage1);
+
+    }
+
+
+}
